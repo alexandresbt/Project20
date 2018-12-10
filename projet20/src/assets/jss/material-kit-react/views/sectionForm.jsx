9dd80b5a4df7eda2d5ae760ca0da24d1310ec0ad
Tacos31 <sabathieralexandre@yahoo.fr>
2018-12-10 18:45:53 +0100

    fix input resize

--- projet20/src/assets/jss/material-kit-react/views/sectionForm.jsx
+++ projet20/src/assets/jss/material-kit-react/views/sectionForm.jsx
@@ -1,9 +1,15 @@
 import { container } from "assets/jss/material-kit-react.jsx";
 
-const sectionForm = {
+const sectionformStyle = {
     container,
-
-
+    input: {
+      marginBottom: "20px",
+      paddingLeft: "5px",
+      paddingRight: "5px"
+    },
+    button: {
+      marginLeft: "20px"
+    }
 };
 
-export default sectionForm;
+export default sectionformStyle;
--- projet20/src/views/Components/Sections/SectionForm.jsx
+++ projet20/src/views/Components/Sections/SectionForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import FormStyle from "assets/jss/material-kit-react/views/sectionForm.jsx";
+import sectionformStyle from "assets/jss/material-kit-react/views/sectionForm.jsx";
 
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
@@ -16,45 +16,56 @@ class SectionForm extends React.Component {
         <div className={classes.title}>
           <h2>Rejoignez l'évènement !</h2>
         </div>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={12} lg={8}>
-            <CustomInput
-              labelText="Nom"
-              id="float"
-              formControlProps={{
-                fullWidth: true
-              }}
-            />
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={12} lg={8}>
-            <CustomInput
-              labelText="Prénom"
-              id="float"
-              formControlProps={{
-                fullWidth: true
-              }}
-            />
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={12} lg={8}>
-            <CustomInput
-              labelText="Adresse mail"
-              id="float"
-              formControlProps={{
-                fullWidth: true
-              }}
-            />
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          <Button color="info">Je m'inscris</Button>
-        </GridContainer>
+        <div className={classes.subtitles}>
+          <h4>Une petite explication</h4>
+        </div>
+        <div className={classes.input}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={12} lg={8}>
+              <CustomInput
+                labelText="Nom"
+                id="float"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+        </div>
+        <div className={classes.input}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={12} lg={8}>
+              <CustomInput
+                labelText="Prénom"
+                id="float"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+        </div>
+        <div className={classes.input}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={12} lg={8}>
+              <CustomInput
+                labelText="Adresse mail"
+                id="float"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+        </div>
+        <div className={classes.button}>
+          <GridContainer>
+            <Button color="info">Je m'inscris</Button>
+          </GridContainer>
+        </div>
       </div>
     );
   }
 }
 
-export default withStyles(FormStyle)(SectionForm);
+export default withStyles(sectionformStyle)(SectionForm);
