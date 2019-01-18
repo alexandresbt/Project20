ccd667155fb2d1ed61206d030fbd00cf7f5402e4
fanaw <julien.galleron@insa-lyon.fr>
2019-01-18 17:24:39 +0100

    gfeef

--- database/app.js
+++ database/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -23,7 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use(function(_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.options('http://tc20ans.insa-lyon.fr:6556/*', function (request, response, next) {
   console.log("COUCOU")
